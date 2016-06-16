85da2bdf891cfc51f8b5d83e32b0a1d1cf70e379
royforweb <royforweb@gmail.com>
2016-06-16 21:45:10 +0300

    new commit

--- src/main/java/com/testapp/ControllerA.java
+++ src/main/java/com/testapp/ControllerA.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerA {
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String greeting( ) {
 
         return "greeting new";
